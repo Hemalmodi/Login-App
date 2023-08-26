33a2a0e060b26e204b17c2a47ea93cb9293d12c2
Hemal <hemalmodi234@gmail.com>
2023-08-26 21:04:54 +0530

    second-Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,55 @@
+import react from 'react'
+import {createBrowserRouter, Router, RouterProvider} from 'react-router-dom'
+// import all componnent
+import Username from './component/Username';
+import Ragister from './component/Ragister';
+import Password from './component/Password';
+import Profile from './component/Profile';
+import Recover from './component/Recover';
+import Reset from './component/Reset';
+import PageNotFound from './component/PageNotFound';
+
+// routes
+const routes = createBrowserRouter([
+  // home page
+  {
+    path: '/',
+    element: <Username></Username>
+  },
+
+  {
+    path: '/ragister',
+    element: <Ragister></Ragister>
+  },
+
+  {
+    path: '/password',
+    element: <Password></Password>
+  },
+  {
+    path: '/profiel',
+    element: <Profile></Profile>
+  },
+  {
+    path: '/recovery',
+    element: <Recover></Recover>
+  },
+  {
+    path: '*',
+    element: <PageNotFound></PageNotFound>
+  },
+  {
+    path: '/reset',
+    element: <Reset></Reset>
+  }
+])
+
 
 function App() {
   return (
-    <div className="App">
-     <><h1>jai hind</h1></>
-     so this is hwo i started my project huree
-    </div>
+    <main>
+      <RouterProvider router={routes}></RouterProvider>
+    </main>
   );
 }
 
---
+++ client/src/component/Password.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Password() {
+  return (
+    <div>
+      hii this is password route.
+    </div>
+  )
+}
+
+export default Password
